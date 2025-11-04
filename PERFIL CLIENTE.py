a1ce4d6f3d9a5715c5e193089f20b5433d4896bb
JOAO VICTOR MOREIRA VALE <joao.vale@aluno.senac.rio>
2025-11-03 21:35:27 -0300

    att

--- PERFIL CLIENTE.py
+++ PERFIL CLIENTE.py
@@ -2,40 +2,36 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 
-df_clientes = pd.read_csv("clientes.csv" , sep = ",", encoding = "latin1")
-df_vendas   = pd.read_csv("vendas.csv"   , sep = ",", encoding = "latin1")
-df_produtos = pd.read_csv("produtos.csv" , sep = ",", encoding = "latin1")
+df_clientes  = pd.read_csv("clientes.csv"   , sep = ";", encoding = "latin1")
+df_vendas    = pd.read_csv("vendas.csv"     , sep = ";", encoding = "latin1")
+df_vendas1   = pd.read_csv("Vendas1.csv"    , sep = ";", encoding = "latin1")
+df_vendas2   = pd.read_csv("Vendas2.csv"    , sep = ";", encoding = "latin1")
+df_vendas3   = pd.read_csv("Vendas3.csv"    , sep = ";", encoding = "latin1")
+df_produtos  = pd.read_csv("produtos.csv"   , sep = ";", encoding = "latin1")
+
 
 # Limpar e padronizar nomes
 def clean_df(df):
     df = df.rename(columns = lambda x: x.strip().lower().replace(" ","_"))
     return df 
 
-df_clientes = clean_df(df_clientes)
-df_vendas   = clean_df(df_vendas)
-df_produtos = clean_df(df_produtos)
+df_clientes   = clean_df (df_clientes)
+df_vendas     = clean_df (df_vendas)
+df_vendas1    = clean_df (df_vendas1)
+df_vendas2    = clean_df (df_vendas2)
+df_vendas3    = clean_df (df_vendas3)
+df_produtos   = clean_df (df_produtos)
 
-# Base Unificada 
-df = df_vendas.merge(df_clientes, on = "id_cliente").merge(df_produtos, on = "id_produto")
-
-# Jovem < 18
-# Adulto 18 ate 60 
-# Idoso 60 + 
+# Base Unificada Vendas 
+vendas = pd.concat([df_vendas,df_vendas1,df_vendas2,df_vendas3],ignore_index = True)
 
 intervalo = [0, 18, 60, 120]
 categoria = ["Jovem", "Adulto", "Idoso"]
 
-df["faixa_etaria"] = pd.cut(df["idade"], bins = intervalo , labels = categoria, right = True)
+df_clientes["faixa_etaria"] = pd.cut(df_clientes["idade"], bins = intervalo , labels = categoria, right = True)
 
-mais_comprados_faixa_etaria_          = df.groupby (["faixa_etaria","id_produto"])       ["valor_total"]        .sum().sort_values (ascending = False)
-frequencia_de_compras_por_canal       = df.groupby ("faixa_etaria")                      ["canal_aquisicao"]    .value_counts      (normalize = True)
-ticket_medio_por_faixa_etaria         = df.groupby (["canal_aquisicao", "faixa_etaria"]) ["valor_total"]        .mean()
-gasto_medio_faixa                     = df.groupby ("faixa_etaria")                      ["valor_total"]        .mean()
-quantidade_media_produtos             = df.groupby("faixa_etaria")                       ["quantidade"]         .mean()
-
-gasto_medio_faixa.plot(kind = "bar", title = "Gasto médio por faixa etária")
-plt.ylabel("Valor médio")
-plt.show()
-
-
-
+'''mais_comprados_faixa                = vendas.groupby (["idade","id_produto"])        ["valor_total"]        .sum().sort_values (ascending = False)
+frequencia_de_compras_por_canal       = vendas.groupby ("idade")                       ["canal_aquisicao"]    .value_counts      (normalize = True)
+ticket_medio_por_idade                = vendas.groupby (["canal_aquisicao", "idade"])  ["valor_total"]        .mean()
+gasto_medio                           = vendas.groupby ("idade")                       ["valor_total"]        .mean()
+quantidade_media_produtos             = vendas.groupby ("idade")                       ["quantidade"]         .mean()'''

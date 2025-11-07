b373061cc3604452a6743e8d1d3221c1f2bf5735
JOAO VICTOR MOREIRA VALE <joao.vale@aluno.senac.rio>
2025-11-07 19:26:58 -0300

    att

---
+++ id campanha.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+df_vendas    = pd.read_csv("vendas.csv"    , sep = ";", encoding = "utf-8-sig")
+df_vendas1   = pd.read_csv("venda 1.csv"   , sep = ";", encoding = "utf-8-sig")
+df_vendas2   = pd.read_csv("venda 2.csv"   , sep = ";", encoding = "utf-8-sig")
+df_vendas3   = pd.read_csv("venda 3.csv"   , sep = ";", encoding = "utf-8-sig")
